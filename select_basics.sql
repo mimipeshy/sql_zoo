644a52286b15a690af95c853070641aeab8dcf8b
mimi peshy <mimipeshy@gmail.com>
2020-07-03 19:03:14 +0300

    select basics queries

---
+++ select_basics.sql
@@ -0,0 +1,15 @@
+-- Modify it to show the population of Germany
+
+SELECT population FROM world
+  WHERE name = 'Germany'
+
+-- Show the name and the population for 'Sweden', 'Norway' and 'Denmark'.
+SELECT name, population FROM world
+  WHERE name IN ('Sweden', 'Norway', 'Denmark');
+
+-- Which countries are not too small and not too big? BETWEEN allows range checking (range specified is inclusive of boundary values).
+-- The example below shows countries with an area of 250,000-300,000 sq. km. Modify it to show the country and the area for countries 
+-- with an area between 200,000 and 250,000.
+
+SELECT name, area FROM world
+  WHERE area BETWEEN 200000 AND 250000
